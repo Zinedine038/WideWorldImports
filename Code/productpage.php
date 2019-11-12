4af0e5c593d7d2dc3e4ad13005896b43e69003fb
TobiasLiefhebber <Tobiasliefhebber@hotmail.com>
2019-11-12 11:49:46 +0100

    Basic styles productpagine
    Dit is een beetje de foundation voor de productpagine totdat ik verder kan.

--- Code/productpage.php
+++ Code/productpage.php
@@ -1,48 +1,79 @@
 <!DOCTYPE html>
 <html lang="en">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
 <?php
-    include "functions.php";
+include "functions.php";
 
-    ///Haalt productnummer uit GET, standaard is 1 om te kunnen testen
-    $productnr=220;
-    if(isset($_GET["stockitemid"])){
-        $productnr=intval($_GET["stockitemid"]);
-    }
+///Haalt productnummer uit GET, standaard is 1 om te kunnen testen
+$productnr = 220;
+if (isset($_GET["stockitemid"])) {
+    $productnr = intval($_GET["stockitemid"]);
+}
 
-    ///Haalt de informatie op uit de database
-    $productnaam = sql("stockitems", "stockitemname", $productnr);
-    $prijs = sql("stockitems", "RecommendedRetailPrice", $productnr);
-    $marketing = sql("stockitems", "MarketingComments", $productnr);
-    $voorraad = sql("stockitemholdings", "QuantityOnHand", $productnr);
-    $gekoeld = sql("stockitems","ischillerstock",$productnr);
-    $foto = sqlfoto($productnr);
+///Haalt de informatie op uit de database
+$productnaam = sql("stockitems", "stockitemname", $productnr);
+$prijs = sql("stockitems", "RecommendedRetailPrice", $productnr);
+$marketing = sql("stockitems", "MarketingComments", $productnr);
+$voorraad = sql("stockitemholdings", "QuantityOnHand", $productnr);
+$gekoeld = sql("stockitems", "ischillerstock", $productnr);
+$foto = sqlfoto($productnr);
 ?>
 <head>
     <meta charset="UTF-8">
     <title>Wide World Importers - <?php print($productnaam); ?></title>
 </head>
 <body>
-<?php
-    print ("$productnaam - €$prijs <br>");
-    if ($marketing) {
-        print ("$marketing<br>");
-    }
-    print ("Voorraad: $voorraad<br>");
+<div class="container">
+    <div class="row">
+        <div class="col" align="center">
+            <?php
+            if ($foto) {
+                print ('<img src="data:image/jpeg;base64,' . base64_encode($foto) . '" /><br>');
+            } else { // zo niet, foto van categorie tonen
+                print ("Afbeelding categorie<br>"); // Waar staan de categoriëen in de db??
+            }
+            ?>
+        </div>
 
-    // Als er een foto in de database van product staat
-    if ($foto) {
-        print ('<img src="data:image/jpeg;base64,'.base64_encode($foto).'" /><br>');
-    } else { // zo niet, foto van categorie tonen
-        print ("Afbeelding categorie<br>"); // Waar staan de categoriëen in de db??
-    }
+        <div class="col">
+            <?php
+            print ("<h5>$productnaam </h5> €$prijs <br>");
+            if ($marketing) {
+                print ("$marketing<br>");
+            }
+            print ("Voorraad: $voorraad<br>");
 
-    if($gekoeld){
-        print ("Product is gekoeld!");
-        print ('<img src="data:image/jpeg;base64,'.base64_encode($foto).'" /><br>');
+            // Als er een foto in de database van product staat
 
-        $temp = sqltemp($productnr);
-        print ("Het product is $temp&deg;");
-    }
-?>
+            if ($gekoeld) {
+                print ("Product is gekoeld!");
+                $temp = sqltemp($productnr);
+                print ("Het product is $temp&deg;");
+            }
+            ?>
+            <br>
+            <button type="button" class="btn btn-primary">Plaats in winkelwagen</button>
+
+
+
+        </div>
+    </div>
+    <br><br><br>
+
+    <div class="row">
+        <div class="col" align="center">
+            <?php
+            if ($foto) {
+                print ('<img src="data:image/jpeg;base64,' . base64_encode($foto) . '" /><br>');
+            } else { // zo niet, foto van categorie tonen
+                print ("Afbeelding categorie<br>"); // Waar staan de categoriëen in de db??
+            }
+            ?>
+        </div>
+    </div>
+</div>
 </body>
 </html>
