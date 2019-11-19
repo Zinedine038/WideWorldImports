dc5389c12546f939370213048c1f9d78f7a7881c
JeMoeder <williamhartm@hotmail.com>
2019-11-19 10:14:28 +0100

    fonts + css + productpagina

--- Code/productpage.php
+++ Code/productpage.php
@@ -10,7 +10,7 @@
             <div class="col-md-7" align="center">
                 <?php
                 if ($foto) {
-                    print ('<img src="' . $foto["0"] . '" /><br>');
+                    print ('<img src="' . $foto["0"] . '" class="product-img"/><br>');
                 } else { // zo niet, foto van categorie tonen
                     print ("Afbeelding categorie<br>"); // Waar staan de categoriëen in de db??
                 }
@@ -18,18 +18,17 @@
             </div>
             <div class="col-md-5">
                 <?php
-                print ("€$prijs <br>");
+                print ("<h3 class='inh'>€$prijs</h3><br>");
+                print ("Voorraad: $voorraad<br>");
                 if ($marketing) {
                     print ("$marketing<br>");
                 }
-                print ("Voorraad: $voorraad<br>");
 
                 // Als er een foto in de database van product staat
-
                 if ($gekoeld) {
-                    print ("Product is gekoeld!");
+                    print ("Product is gekoeld! ");
                     $temp = sqltemp($productnr);
-                    print ("Het product is $temp&deg;");
+                    print ("Het product is $temp&deg;.");
                 }
                 ?>
                 <br>
@@ -43,7 +42,7 @@
         <div class="container rel-prod pt-3 pb-3">
             <div class="row">
                 <div class="col">
-                    <h2>Relevante producten</h2>
+                    <h2 class="inh">Gerelateerde producten</h2>
                 </div>
             </div>
             <div class="row">
