8eaf71d672b097bc211e36bf02e915f7e63b0a8d
JeMoeder <williamhartm@hotmail.com>
2019-11-19 09:40:45 +0100

    bewerking productpagina + css

--- Code/productpage.php
+++ Code/productpage.php
@@ -1,49 +1,64 @@
 <?php include 'header.php' ?>
 
-<div class="container content">
-    <div class="row">
-        <div class="col" align="center">
-            <?php
-            if ($foto) {
-                print ('<img src="'.$foto["0"].'" /><br>');
-            } else { // zo niet, foto van categorie tonen
-                print ("Afbeelding categorie<br>"); // Waar staan de categoriëen in de db??
-            }
-            ?>
+    <div class="container content">
+        <div class="row">
+            <div class="col-12">
+                <h1><?php print ($productnaam) ?></h1>
+            </div>
         </div>
+        <div class="row">
+            <div class="col-md-7" align="center">
+                <?php
+                if ($foto) {
+                    print ('<img src="' . $foto["0"] . '" /><br>');
+                } else { // zo niet, foto van categorie tonen
+                    print ("Afbeelding categorie<br>"); // Waar staan de categoriëen in de db??
+                }
+                ?>
+            </div>
+            <div class="col-md-5">
+                <?php
+                print ("€$prijs <br>");
+                if ($marketing) {
+                    print ("$marketing<br>");
+                }
+                print ("Voorraad: $voorraad<br>");
 
-        <div class="col">
-            <?php
-            print ("<h5>$productnaam </h5> €$prijs <br>");
-            if ($marketing) {
-                print ("$marketing<br>");
-            }
-            print ("Voorraad: $voorraad<br>");
+                // Als er een foto in de database van product staat
 
-            // Als er een foto in de database van product staat
-
-            if ($gekoeld) {
-                print ("Product is gekoeld!");
-                $temp = sqltemp($productnr);
-                print ("Het product is $temp&deg;");
-            }
-            ?>
-            <br>
-            <button type="button" class="btn btn-primary">Plaats in winkelwagen</button>
-        </div>
-    </div>
-    <br><br><br>
-    <div class="row">
-        <div class="col" align="center">
-            <?php
-            if ($foto) {
-                print ('<img src="data:image/jpeg;base64,' . base64_encode($foto) . '" /><br>');
-            } else { // zo niet, foto van categorie tonen
-                print ("Afbeelding categorie<br>"); // Waar staan de categoriëen in de db??
-            }
-            ?>
+                if ($gekoeld) {
+                    print ("Product is gekoeld!");
+                    $temp = sqltemp($productnr);
+                    print ("Het product is $temp&deg;");
+                }
+                ?>
+                <br>
+                <button type="button" class="btn btn-primary">Plaats in winkelwagen</button>
+            </div>
         </div>
     </div>
-</div>
+
+    <!--- START RELEVANTE PRODUCTEN --->
+    <div class="container-fluid bg">
+        <div class="container rel-prod pt-3 pb-3">
+            <div class="row">
+                <div class="col">
+                    <h2>Relevante producten</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    GERELATEERD PRODUCT 1
+                </div>
+                <div class="col-md-4">
+                    GERELATEERD PRODUCT 2
+                </div>
+                <div class="col-md-4">
+                    GERELATEERD PRODUCT 3
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--- END RELEVANTE PRODUCTEN --->
 
 <?php include 'footer.php' ?>
