f337e36ef9ed3f717d3a1d1243a50d2f9f02400c
Zinedine038 <zsghari@hotmail.com>
2019-11-19 08:53:13 +0100

    trying to fix shit

--- Code/livesearchbalk.php
+++ Code/livesearchbalk.php
@@ -1,25 +1,33 @@
-<script>
-    function showResult(str) {
-        if (str.length == 0) {
-            document.getElementById("livesearch").innerHTML = "";
-            document.getElementById("livesearch").style.border = "0px";
-            return;
-        }
-        if (window.XMLHttpRequest) {
-            // code for IE7+, Firefox, Chrome, Opera, Safari
-            xmlhttp = new XMLHttpRequest();
-        } else {  // code for IE6, IE5
-            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        xmlhttp.onreadystatechange = function () {
-            // Deze functie wordt alleen aangeroepen als zijn status veranderd.
-            if (this.readyState == 4 && this.status == 200) {
-                // Geeft alleen resultaat als het verzoek om informatie te krijgen gelukt is aan de client-kant en server-kant.
-                document.getElementById("livesearch").innerHTML = this.responseText;
-                // Zet het resultaat van de zoekopdracht in de (zichtbare) zoekbalk.
-                document.getElementById("livesearch").style.border = "1px solid #76eec6";
-                // Geeft de resultatenbalk een rand eromheen.
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Zoek product</title>
+    <script>
+        function showResult(str) {
+            if (str.length==0) {
+                document.getElementById("livesearch").innerHTML="";
+                document.getElementById("livesearch").style.border="0px";
+                return;
             }
+            if (window.XMLHttpRequest) {
+                // code for IE7+, Firefox, Chrome, Opera, Safari
+                xmlhttp=new XMLHttpRequest();
+            } else {  // code for IE6, IE5
+                xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+            }
+            xmlhttp.onreadystatechange=function() { 
+                    // Deze functie wordt alleen aangeroepen als zijn status veranderd.
+                if (this.readyState==4 && this.status==200) {
+                    // Geeft alleen resultaat als het verzoek om informatie te krijgen gelukt is aan de client-kant en server-kant.
+                    document.getElementById("livesearch").innerHTML=this.responseText;
+                    // Zet het resultaat van de zoekopdracht in de (zichtbare) zoekbalk.
+                    document.getElementById("livesearch").style.border="1px solid #76eec6";
+                    // Geeft de resultatenbalk een rand eromheen.
+                }
+            }
+            // Stuur het resultaat door naar de pagina livesearch.php
+            xmlhttp.open("GET","livesearch.php?q="+str,true);
+            xmlhttp.send();
         }
     </script>
 </head>
@@ -40,8 +48,5 @@
     <div id="livesearch" style='width: 40%;'></div>
 </form>
 
-<div class="md-form">
-    <input class="form-control" type="text" size="30" onkeyup="showResult(this.value)" placeholder="Zoeken"
-           aria-label="Search">
-    <div id="livesearch"></div>
-</div>
+</body>
+</html>
