3d4ef9c635a0171d1626229b822d4c4d1d6ec3f2
Anonymoesje <50021531+Anonymoesje@users.noreply.github.com>
2019-12-03 22:42:07 +0100

    Kan niet meer nadenken, alles lijkt te werken.
    Na voor de 2de keer alles opnieuw te bouwen ben erachter gekomen dat php wel in javascript kan maar javascript niet in php, en php in javascript in php in javascript werkt niet. Verder kan een formulier niet in een ander formulier zitten, dus moet het wel appart. Misschien voor iemand interessant: document.getElementById("idvaniets").value=<?php print($phpvariabele);?> hiermee kan je in een form een input een waarde geven met informatie uit php. Code is wel shit maar lijkt te werken. Direct ook een conversieverhogende maatregel toegepast, wachtwoord moet minimaal uit 8 tekens bestaan, en moet 1 hoofdletter, kleine letter, cijfer en ander karakter bevatten.

--- Code/inloggen.php
+++ Code/inloggen.php
@@ -4,31 +4,59 @@ MOET FORMS EIGEN ID GEVEN WERKT DAAROM NU NIET
 
 <?php
     include "header.php";
-    $postcode = "";
+    $postcode="";
     $huisnummer="";
     $plaats="";
-    /// Zet variabelen naar user input en haalt de eventuele spaties weg, maakt de postcode upper case
-    if (isset($_GET["submit"])) {
-        $postcode =  strtoupper(str_replace(" ","",$_GET["postcode"]));
+
+ if (isset($_GET["huisnummer"]) || isset($_GET["postcode"])) {
+        /// Zet variabelen naar user input en haalt de eventuele spaties weg, maakt de postcode upper case
+        $postcode = strtoupper(str_replace(" ", "", $_GET["postcode"]));
         $huisnummer = trim($_GET["huisnummer"]);
-        $plaats = trim($_GET["gemeentenaam"]);
     }
 
-    if(isset($_GET["postcode"]) AND isset($_GET["huisnummer"])) {
-        $resultaten = zoekadres($postcode,$huisnummer);
+if (isset($_GET["submit"])) {
+    if ($postcode == "" || $huisnummer == "") {
+        print("<h1 style='color: red; text-align: center; background-color: #00fafa'>Geef je huisnummer en postcode!</h1>");
+    }
+}
+
+if  ($postcode!="" && $huisnummer!="") {
+    $resultaten = zoekadres($postcode,$huisnummer);
 /// Controleerd of er resultaten gevonden zijn
-        if($resultaten!=0) {
-            $straat = $resultaten["straatnaam"];
-            $plaats = $resultaten["gemeentenaam"];
-        }
-        else{
-            print("Deze deze combinatie is niet gevonden!");
-        }}
+    if($resultaten!=0) {
+        $straat = $resultaten["straatnaam"];
+        $straat="$straat $huisnummer";
+        $plaats = $resultaten["gemeentenaam"];
+    }
+    else{
+        print("<h1 style='color: red; text-align: center; background-color: #00fafa'>Adres niet gevonden, geef een geldige postcode en huisnummer op!</h1>");
+        //error, wordt al weergeven bij bovenstaande functie.
+    }}
+
+
+
+
+
     ?>
     <div class="container">
     <h2>Account aanmaken</h2>
 
-    <form action="inloggen.php" method="post">
+        <form action="inloggen.php" method="get">
+            Huisnummer
+            <div class="form-group">
+                <input type="number" value="<?php if (isset ($huisnummer)) {print $huisnummer;}?>" name="huisnummer" placeholder="Typ hier je Huisnummer" class="form-control input-lg" required>
+            </div>
+
+
+            Postcode
+            <div class="form-group">
+                <input type="text" value="<?php if (isset ($postcode)) {print $postcode;} ?>"
+                       name="postcode" placeholder="Typ hier je Postcode" class="form-control input-lg" required>
+            </div>
+            <input type="submit" name="submit" value="Vul automatisch in" class="btn btn-secondary">
+        <br>
+        </form>
+    <form action="verzenden.php" method="get">
         <form method="get" action="productpage.php">
         <div class="row" style="width: 90%; padding: 5%">
 
@@ -37,33 +65,33 @@ MOET FORMS EIGEN ID GEVEN WERKT DAAROM NU NIET
                 <div class="form-group">
                 <div class="form-group">
                     Voornaam
-                    <input type="text" name="voornaam" placeholder="Typ hier je voornaam" class="form-control input-lg">
+                    <input type="text" name="voornaam" placeholder="Typ hier je voornaam"
+                           class="form-control input-lg" required>
                 </div>
-                    <?php
-                    print ($_GET["postcode"]);
-                    ?>
+
                     <div class="form-group">
                         Tussenvoegsel
-                        <input type="text" name="tussenvoegsel" placeholder="Typ hier je tussenvoegsel"
+                        <input type="text" name="tussenvoegsel" placeholder="Typ hier je tussenvoegsel" required
                                class="form-control input-lg">
                     </div>
 
 
                     <div class="form-group">
                         Achternaam
-                        <input type="text" name="achternaam" placeholder="Typ hier je achternaam"
+                        <input type="text" name="achternaam" placeholder="Typ hier je achternaam" required
                                class="form-control input-lg">
                     </div>
 
 
                     <div class="form-group">
                         Email
-                        <input type="text" name="email" placeholder="Typ hier je email-adres" class="form-control input-lg">
+                        <input type="text" name="email" placeholder="Typ hier je email-adres" required
+                               class="form-control input-lg">
                     </div>
 
                     Wachtwoord
                     <div class="form-group">
-                        <input type="text" name="wachtwoord" placeholder="Typ hier je wachtwoord"
+                        <input title="Wachtwoord moet minimaal uit 8 tekens bestaan, en moet 1 hoofdletter, kleine letter, cijfer en ander karakter bevatten!" type="password" name="wachtwoord" placeholder="Typ hier je wachtwoord" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
                                class="form-control input-lg">
                     </div>
                 </div>
@@ -74,30 +102,16 @@ MOET FORMS EIGEN ID GEVEN WERKT DAAROM NU NIET
                 <div class="col-md-6">
                 Straatnaam
                 <div class="form-group">
-                    <input type="text" value="<?php print $plaats ?>" name="straatnaam" placeholder="Typ hier je Straatnaam" class="form-control input-lg">
+                    <input type="text" value="<?php if (isset ($straat)) {print $straat;}?>" name="straatnaam" placeholder="Typ hier je Straatnaam" readonly
+                           class="form-control input-lg">
                 </div>
-
-
-
-                Huisnummer
-                <div class="form-group">
-                    <input type="number" value="<?php print $huisnummer ?>" name="huisnummer" placeholder="Typ hier je Huisnummer" class="form-control input-lg">
-                </div>
-
-
-                Postcode
-                <div class="form-group">
-                    <input type="text" value="<?php print $postcode ?>"
-                           name="postcode" placeholder="Typ hier je Postcode" class="form-control input-lg">
-                </div>
-                        <input type="submit" name="submit" value="Vul automatisch in" class="btn btn-secondary"><br>
-
 
 
                     Plaats
                     <div class="form-group">
                         <input type="text" value="<?php if(isset($plaats)) {print($plaats);}?>"
-                               name="gemeentenaam" placeholder="Typ hier je Plaats" class="form-control input-lg">
+                               name="gemeentenaam" placeholder="Typ hier je Plaats" readonly
+                               class="form-control input-lg">
                     </div>
 
 
