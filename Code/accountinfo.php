407bf40b04a3f24936a68d4cad99bc069d6b7652
Nothingmeta <the_sjoerd_@hotmail.com>
2019-12-10 13:52:15 +0100

    Accountinfo gegevens "werkend"

--- Code/accountinfo.php
+++ Code/accountinfo.php
@@ -3,12 +3,12 @@ session_start();
 
 include "header.php";
 // MOET IN HEADER
-if (!(isset($_SESSION["ingelogd"]))) {
+if (!(isset($_SESSION["voornaam"]))) {
     $_SESSION["ingelogd"] = false;
 }
 ?>
 
 
-<?php if ($_SESSION["ingelogd"] == true) {
-    print($_SESSION["voornaam"]);
+<?php if ($_SESSION["voornaam"] == true) {
+    print_r($_SESSION);
 }
