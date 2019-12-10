ed08487abb5c852cc753b1b64c95f8e0c328d189
JeMoeder <williamhartm@hotmail.com>
2019-12-10 13:25:16 +0100

    footer en over onspagina

--- Code/footer.php
+++ Code/footer.php
@@ -1,3 +1,25 @@
+<?php if (isset($_GET["stockitemid"])) { ?>
+<div class="container">
+    <div class="row my-4">
 
+<?php } else { ?>
+<div class="container-fluid bg mt-4 p-4">
+    <div class="container">
+        <div class="row">
+<?php } ?>
+        <div class="col-md-3 text-sm-center">
+            <img src="images/ideal.png" class="footer-img" alt="ideal logo" />
+            <img src="images/266px-Mastercard-logo.svg.png" class="footer-img" alt="mastercard logo" />
+            <img src="images/Visa.png" class="footer-img" alt="visa logo" />
+        </div>
+        <div class="col-md-3 text-sm-center">
+            <img src="images/wecycle-logo.jpg" class="footer-img" alt="wecycle logo" />
+            <img src="images/Thuiswinkel_Org_Kleur_Horizontaal.png" class="footer-img" alt="thuiswinkel logo" />
+        </div>
+        <div class="col-md-3">
+
+        </div>
+    </div>
+</div>
 </body>
 </html>
---
+++ Code/overons.php
@@ -0,0 +1,22 @@
+<?php include 'header.php'; ?>
+
+<div class="container pt-5">
+    <div class="row">
+        <div class="col-md-6">
+            <h2>Wie zijn wij</h2>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus egestas fermentum sodales. Curabitur metus lorem, molestie vel suscipit nec, tempus ac ex. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque placerat congue eleifend. Proin nisl neque, elementum nec egestas et, porttitor tincidunt nisi. Suspendisse efficitur sapien rhoncus ipsum pretium, in hendrerit ipsum finibus. Sed rhoncus a nisl vel posuere. Curabitur in porttitor enim.<br>
+            <br>
+            Donec a odio purus. Praesent volutpat turpis at lacus pharetra aliquam. Nunc dignissim accumsan varius. Phasellus tincidunt commodo nulla, at hendrerit sapien mollis eget. Pellentesque gravida scelerisque dignissim. Sed accumsan, mi at vehicula faucibus, orci purus varius risus, non gravida tortor neque at libero. Duis ullamcorper et enim vel euismod. Nulla nisi augue, gravida nec justo sed, suscipit tincidunt purus. Duis malesuada ut nisl rutrum porttitor. Maecenas vehicula aliquet lacus sed placerat. Aliquam tristique lorem odio, vel accumsan tellus ultrices sed.<br>
+            <br>
+            Praesent tincidunt lectus ultricies aliquet blandit. Quisque id urna diam. Aliquam dui libero, scelerisque at velit a, feugiat mattis turpis. Integer vulputate commodo mauris laoreet blandit. Nullam bibendum neque eget mauris rutrum, in bibendum diam blandit. Phasellus tortor justo, fringilla a risus et, ullamcorper aliquet metus. Integer vitae consectetur elit. Morbi ante justo, fermentum id ligula quis, suscipit mattis leo. Pellentesque interdum odio eu congue sagittis. Fusce hendrerit rutrum orci, quis scelerisque tortor pellentesque vel.
+        </div>
+        <div class="col-md-6">
+            <h2>Oog voor u</h2>
+            In massa odio, mattis vitae fringilla nec, condimentum eget neque. Nam dictum condimentum molestie. Vivamus placerat erat eu lectus luctus, non volutpat magna mollis. Nam id est diam. Donec ut orci efficitur, commodo est ut, bibendum eros. In iaculis dolor ut turpis imperdiet, in volutpat dolor viverra. Aenean urna nibh, faucibus eget viverra sed, mattis quis eros. Etiam cursus ac elit id rutrum. Aenean in pretium elit. Sed rhoncus lorem sit amet magna ultrices, aliquet auctor eros luctus. Nam accumsan tortor nisl, a dictum quam porttitor at. Nam congue vulputate lacus, sed varius elit laoreet id. Morbi elit nisi, laoreet ut ultricies ac, porttitor vel leo. Praesent rhoncus odio eu molestie congue. Donec vel ante iaculis, vestibulum dolor non, feugiat est. Duis sagittis rhoncus augue, vel pulvinar ex sodales id.<br>
+            <br>
+            Pellentesque fermentum bibendum dui at mollis. Nunc efficitur arcu eu orci condimentum porta at sed nibh. In laoreet ornare massa, et laoreet mauris. Proin vel elit euismod, volutpat eros nec, dapibus lacus. Nullam id urna magna. Nulla imperdiet mauris a placerat dignissim. Ut laoreet blandit quam in semper. Nunc eget enim dolor. Vivamus quis efficitur felis, sit amet vestibulum turpis. Integer id interdum leo.
+        </div>
+    </div>
+</div>
+
+<?php include 'footer.php'; ?>
