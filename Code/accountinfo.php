06392e84addafcb2d5a25d38f7b52bda8436e940
Anonymoesje <50021531+Anonymoesje@users.noreply.github.com>
2019-12-10 14:02:28 +0100

    Testaccount

--- Code/accountinfo.php
+++ Code/accountinfo.php
@@ -5,14 +5,28 @@ include "header.php";
 ?>
 
 <?php if (isset($_SESSION["voornaam"])) {
+    print("Voornaam:");
     print ($_SESSION["voornaam"]);
+    print ("<br>Tussenvoegsel:");
     print ($_SESSION["tussenvoegsel"]);
+    print ("<br>Achternaam:");
     print ($_SESSION["achternaam"]);
+    print("<br>Email:");
     print ($_SESSION["email"]);
+    print("<br>Huisnummer:");
     print ($_SESSION["huisnummer"]);
+    print("<br>Huisnummer toevoegsel:");
     print ($_SESSION["annex"]);
+    print("<br>Straatnaam:");
     print ($_SESSION["straatnaam"]);
+    print("<br>Plaats:");
     print ($_SESSION["plaats"]);
+    print("<br>Postcode:");
     print ($_SESSION["postcode"]);
-    print ($_SESSION["newsletter"]);
+    print("<br>Wilt spam:");
+    if ($_SESSION["newsletter"]==1) {
+        print("Ja");
+    } else {
+        print("Nee");
+    }
 }
