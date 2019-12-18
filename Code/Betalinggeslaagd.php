3e077101da3d9eb11b6ae452c6f8a88af2191621
Anonymoesje <50021531+Anonymoesje@users.noreply.github.com>
2019-12-18 10:39:10 +0100

    Betaling geslaagd

--- Code/Betalinggeslaagd.php
+++ Code/Betalinggeslaagd.php
@@ -1,12 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>$Title$</title>
-</head>
-<body>
+<?php
+include "header.php";
+?>
 <h1>Betaling geslaagd!!!</h1>
 <img src="https://www.alkmaarsdagblad.nl/image/15352_16708_1200_1200.jpg">
 
-</body>
-</html>
+<?php
+include "footer.php";
+?>
