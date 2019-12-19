f7029e4347158d703b7eafeb1156a7f93d1ddb68
Anonymoesje <50021531+Anonymoesje@users.noreply.github.com>
2019-12-19 20:08:50 +0100

    Simpele Betaling

--- Code/Betalinggeslaagd.php
+++ Code/Betalinggeslaagd.php
@@ -2,8 +2,7 @@
 session_start();
 include "header.php";
 ?>
-<h1>Betaling geslaagd!!!</h1>
-<img src="https://www.alkmaarsdagblad.nl/image/15352_16708_1200_1200.jpg">
+    <h1 style="text-align: center">Betaling geslaagd, bedankt voor uw aankoop!<br><br><br></h1>
 
 <?php
 emptyShoppingCart();
