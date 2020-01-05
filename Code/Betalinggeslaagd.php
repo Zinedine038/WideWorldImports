78223506594c422e183f89101907e5013f577517
Anonymoesje <50021531+Anonymoesje@users.noreply.github.com>
2020-01-05 12:04:43 +0100

    Comment betalinggeslaagdpagina

--- Code/Betalinggeslaagd.php
+++ Code/Betalinggeslaagd.php
@@ -1,10 +1,14 @@
 <?php
+// Importeert benodigde header en start sessie.
 session_start();
 include "header.php";
 ?>
+<!-- Geeft een melding dat betaling geslaagd is -->
+
     <h1 style="text-align: center">Betaling geslaagd, bedankt voor uw aankoop!<br><br><br></h1>
 
 <?php
+// Maakt de winkelmand leeg en importeert de footer.
 emptyShoppingCart();
 include "footer.php";
 ?>
